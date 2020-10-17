6f7df2d66f361ed14898fed9cdf9751e827a47b6
zengyuzhi <shbyku@gmail.com>
2020-10-17 16:25:05 +0800

    init 初始化服务

---
+++ mall-promotion/src/main/java/com/dangdang/bookmall/promotion/MallPromotionApplication.java
@@ -0,0 +1,13 @@
+package com.dangdang.bookmall.promotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallPromotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallPromotionApplication.class, args);
+	}
+
+}
---
+++ mall-promotion/src/test/java/com/dangdang/bookmall/promotion/MallPromotionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dangdang.bookmall.promotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallPromotionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
