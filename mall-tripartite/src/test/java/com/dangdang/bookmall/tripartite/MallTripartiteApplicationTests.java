5b7b631cb531fcf5c7573891f5ee3b19b9d2df9a
zengyuzhi <shbyku@gmail.com>
2020-10-21 18:41:25 +0800

    新增OSS服务

--- mall-tripartite/src/test/java/com/dangdang/bookmall/tripartite/MallTripartiteApplicationTests.java
+++ mall-tripartite/src/test/java/com/dangdang/bookmall/tripartite/MallTripartiteApplicationTests.java
@@ -16,14 +16,6 @@ class MallTripartiteApplicationTests {
 
 	@Test
 	void testUpload() throws FileNotFoundException {
-//		// Endpoint以杭州为例，其它Region请按实际情况填写。
-//		String endpoint = "oss-cn-hangzhou.aliyuncs.com";
-//// 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-//		String accessKeyId = "LTAI4GGAsG4FgoKpE3Ct1Nyd";
-//		String accessKeySecret = "NhCANzAJz0gZHFprQGEF8x559s7fEK";
-//
-//// 创建OSSClient实例。
-//		OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
 
 // 上传文件流。
 		InputStream inputStream = new FileInputStream("/home/fengjianche/Pictures/Wallpapers/wallhaven-392qey_1920x1080.png");
