3786f87c6b1b6b722b66fd1b9930536699741950
chenyufeng <1776908657@qq.com>
2020-11-03 11:14:48 +0800

    提交最后

--- mall-order/src/main/java/com/dangdang/bookmall/order/dao/StatisticsDao.java
+++ mall-order/src/main/java/com/dangdang/bookmall/order/dao/StatisticsDao.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Repository
 public interface StatisticsDao {
 
-    @Select("SELECT a.book_name,a.book_num FROM omt_bookinfo a JOIN omt_orderinfo b ON a.order_id = b.`id` where DATE_FORMAT(time_xd,'%Y%m') = DATE_FORMAT(CURDATE(),'%Y%m') ORDER BY a.book_num ASC")
+    @Select("SELECT a.book_name,sum(a.book_num) as book_num FROM omt_bookinfo a JOIN omt_orderinfo b ON a.order_id = b.`id` where DATE_FORMAT(time_xd,'%Y%m') = DATE_FORMAT(CURDATE(),'%Y%m') group by a.book_name ORDER BY a.book_num ASC")
 
     List<SalesrRankingOfTheMonthDto> getSalesrRankingOfTheMonth();
 
